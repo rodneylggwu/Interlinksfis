dc066835f29f12f1802fdda538282549391fa68b
emkk4 <150754649+emkk4@users.noreply.github.com>
2024-04-28 19:06:25 -0400

    Add files via upload

---
+++ ethiopia.php
@@ -0,0 +1,51 @@
+<main id="ethiopia">
+
+<div class="container-fluid">
+<div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-10">
+        <br>
+        <h1 class="text-center">Ethiopia</h1><br>
+        <p class="text-start">The Ethiopian community in the DMV is a vibrant and close-knit cultural enclave, pulsating with rich traditions, diverse backgrounds, and a shared sense of heritage. Nestled within the bustling neighborhoods of the region, this community serves as a hub of connection, celebration, and support for Ethiopians and those interested in Ethiopian culture alike.</p>
+        <h3 class="text-start">Grocery Stores</h3>
+        <ul class="text-start">
+            <li>Abyssinia Market - Offering a wide array of authentic Ethiopian spices, grains, and specialty foods.</li>
+            <li>Addis International Market - Providing fresh produce, injera, and traditional Ethiopian ingredients.</li>
+            <li>Sheba Market - A beloved spot for Ethiopian staples, including teff flour, berbere, and injera.</li>
+        </ul>
+        <h3 class="text-start">Clinics</h3>
+        <ul class="text-start">
+            <li>Ethiopian Community Health Center - Dedicated to providing culturally competent healthcare services, including medical, dental, and mental health support.</li>
+            <li>Habesha Medical Clinic - Specializing in serving the healthcare needs of the Ethiopian community, with a focus on preventive care and wellness.</li>
+        </ul>
+        <h3 class="text-start">Gatherings</h3>
+        <ul class="text-start">
+            <li>Ethiopian Orthodox Church - A spiritual and community focal point, hosting religious services, cultural events, and gatherings for members.</li>
+            <li>Ethiopian Community Center - A hub for social gatherings, cultural celebrations, and educational events, fostering a sense of unity and belonging.</li>
+            <li>Coffee ceremonies - Traditional Ethiopian coffee ceremonies often serve as informal gatherings, where community members come together to share stories, laughter, and friendship over freshly brewed coffee.</li>
+        </ul>
+        <h3 class="text-start">Restaurants</h3>
+        <ul class="text-start">
+            <li>Bethel Cafe: 3045 Columbia Pike, Arlington, VA 22204</li>
+            <li>Enjera Restaurant: 549 23rd St S, Arlington VA 22202</li>
+            <li>Nazret Ethiopian Restaurant: 3821 S George Mason Dr D, Falls Church, VA 22041</li>
+        </ul>
+        <h3 class="text-start">Language and Cultural Classes </h3>
+        <p class="text-start">Various organizations within the community offer language courses, cultural workshops, and educational programs to preserve and promote Ethiopian heritage.</p>
+        <ul class="text-start">
+            <li>Abugida Language and Cultural Center: 7003 Backlick CT, Springfield, VA 22151</li>
+            <li>Ethiopian Community Development Council, Inc: 901 S Highland St, Arlington, VA 22205</li>
+        </ul>
+        <h3 class="text-start">Community</h3>
+        <p class="text-start">Throughout the year, the community organizes sports tournaments, music festivals, and cultural celebrations, providing opportunities for connection, recreation, and showcasing Ethiopian talent and creativity.</p>
+        <ul class="text-start">
+            <li>Ethio Silver Spring Soccer Academy: 8120 Fenton St, Silver Spring, MD 20910 </li>
+        </ul>
+    </div>
+    <div class="col-md-1"></div>
+</div>
+</div>
+
+
+
+</main>
