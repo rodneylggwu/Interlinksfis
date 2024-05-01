510e7543002fb034e1d824be243109a0b34ba27c
emkk4 <150754649+emkk4@users.noreply.github.com>
2024-05-01 12:45:12 -0400

    Add files via upload

--- account.php
+++ account.php
@@ -67,7 +67,7 @@ $dbname = "Interlink";
 
 $uname = $_SESSION['UniversityEmail'];
 
-$conn = new mysqli($servername, $username, $password, $dbname)//, $port);
+$conn = new mysqli($servername, $username, $password, $dbname);//, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
