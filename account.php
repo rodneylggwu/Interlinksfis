a13185d07ca886af3ebc27fa0f8c0a22422a407b
rodneylggwu <rodneylg@gwu.edu>
2024-05-01 13:06:55 -0400

    account page update

--- account.php
+++ account.php
@@ -8,48 +8,6 @@
     <title>Account</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="index.html" style="color: whitesmoke;">Interlink</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="academicadvising.html" style="color: whitesmoke;">Academic Advising</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="events.php" style="color: whitesmoke;">Events</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="yourcommunity.php" style="color: whitesmoke;">Your Community</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="transportation.html" style="color: whitesmoke;">Transportation</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="healthcare.html" style="color: whitesmoke;">HealthCare</a>
-              </li>
-              <li class="nav-item">
-                <a href="contact.php" class="nav-link" style="color: whitesmoke;" >Contact Us</a>
-              </li>
-            </ul>
-            <ul class="navbar-nav justify-content-end">
-            <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="account.php" style="color: whitesmoke;">Account</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="login.php" style="color: whitesmoke;">Login</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="logout.php" style="color: whitesmoke;">Logout</a>
-                </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
     
 <?php
 session_start();
